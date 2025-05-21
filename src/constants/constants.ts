d6841fef1cea27cbde76b6695cc9283f533ef8ac
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-05-21 10:13:36 +0000

    Add database connection logic and global mongoose type definitions

---
+++ src/constants/constants.ts
@@ -0,0 +1,4 @@
+const DB_NAME = "Tifinz";
+const DEFAULT_AVATAR_URI = "frggfrhg"
+
+export {DB_NAME, DEFAULT_AVATAR_URI}
---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants/constants";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI env not found");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = { connection: null, promise: null };
+}
+
+const connectDB = async () => {
+  if (cached.connection) return cached.connection;
+
+  if (!cached.promise) {
+    const options = { bufferCommands: true, maxPoolSize: 10 };
+    cached.promise = mongoose
+      .connect(`${MONGODB_URI}/${DB_NAME}`, options)
+      .then((m) => m.connection);
+  }
+
+  try {
+    cached.connection = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.error("Failed to connect with db ERROR :", error);
+    throw error;
+  }
+  return cached.connection;
+};
+
+export default connectDB;
