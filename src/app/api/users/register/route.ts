4c94e19a79bf16264d24571e6e35751345024511
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-05-22 10:18:27 +0000

    Refactor user registration API to streamline user existence check and remove unused GET method

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -4,34 +4,15 @@ import { ApiResponse } from "@/lib/ApiResponse";
 import connectDB from "@/lib/dbConnect";
 import User, { IUser } from "@/models/user.model";
 import { userSchema } from "@/zod/user.schema";
-import { MongooseError } from "mongoose";
-import { NextRequest } from "next/server";
+import { asyncHandler } from "@/utils/asyncHandler";
 
-export async function GET() {
-  try {
-    console.log("HEllo  ");
-    const connection = await connectDB();
-    console.log(`Db connection success host:${connection.host}`);
-
-    return ApiResponse.success(
-      "DB connected success fully",
-      { host: connection.host },
-      200
-    );
-  } catch (error) {
-    console.error("Connect failed ", error);
-    return ApiResponse.error(`Failed to connect with DB`, 500, error);
-  }
-}
-
-export async function POST(req: NextRequest) {
+export const POST = asyncHandler(async (req) => {
   const body: typeof userSchema = await req.json();
   const parseResult = userSchema.safeParse(body);
   if (!parseResult.success) {
     return ApiResponse.zodError(parseResult.error);
   }
   const userData = parseResult.data;
-
   if (
     userData.role === UserRole.admin &&
     userData?.adminSecret !== process.env.ADMIN_SECRET!
@@ -41,40 +22,43 @@ export async function POST(req: NextRequest) {
     );
     userData.role = UserRole.user;
   }
-  try {
-    await connectDB();
-    const existingUser = await User.findOne({ phone: userData.phone });
-    if (existingUser) {
-      return ApiResponse.error("User with phone number already registered");
-    }
 
-    console.log(userData?.email);
-    const newUser: IUser = {
-      username: userData.username,
-      fullName: userData.fullName,
-      phone: userData.phone,
-      avatar: userData.avatar || DEFAULT_AVATAR_URI,
-      password: userData.password,
-      role: userData.role,
-      ...(userData?.email && { email: userData.email }),
-    };
-    const userDoc = await User.create(newUser);
-    const createdUser = await User.findById(userDoc._id).select("-password");
-    if (!createdUser) {
-      throw ApiResponse.error("Failed to register user", 500);
-    }
-    return ApiResponse.success(
-      `User ${userData.role === UserRole.admin && "Admin"} registered successfully `,
-      { user: createdUser },
-      201
-    );
-  } catch (error) {
-    // console.log(error)
-    if (error instanceof MongooseError) {
-      return ApiResponse.error(error.message, 400);
-    }
-    const message =
-      error instanceof Error ? error.message : "Internal server error";
-    return ApiResponse.error(message, 500);
+  await connectDB(); //db connection
+
+  // conditions for finding user by phone, username, email if provided
+  const orConditions: Array<Record<string, string>> = [
+    { phone: userData.phone },
+    { username: userData.username },
+  ];
+
+  if (userData?.email) {
+    orConditions.push({ email: userData.email });
   }
-}
+  const existingUser = await User.findOne({ $or: orConditions }); //checking if user already exists or not
+
+  if (existingUser) {
+    const credential = userData?.email ? "Email" : "phone number/username";
+    return ApiResponse.error(`User with ${credential} already registered`);
+  }
+
+  // console.log(userData?.email);
+  const newUser: IUser = {
+    username: userData.username,
+    fullName: userData.fullName,
+    phone: userData.phone,
+    avatar: userData.avatar || DEFAULT_AVATAR_URI,
+    password: userData.password,
+    role: userData.role,
+    isVeriffied: userData.role === UserRole.admin,
+    ...(userData?.email && { email: userData.email }),
+  };
+  const userDoc = await User.create(newUser);
+  const createdUser = await User.findById(userDoc._id).select("-password");
+  if (!createdUser) {
+    throw ApiResponse.error("Failed to register user", 500);
+  }
+   const userType = userData.role === UserRole.admin ? "Admin" : "User"
+  return ApiResponse.success(`${userType} registered successfully `,
+    { user: createdUser },
+    201);
+});
