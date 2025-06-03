e5ed6cfd1f25b68a10157744758e9014d86ab07e
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-03 08:39:17 +0000

    Refactor user account service to improve function structure and add balance request validation using Zod schema

--- src/services/server/user.accountService.ts
+++ src/services/server/user.accountService.ts
@@ -2,9 +2,12 @@ import Account, { IAccount } from "@/models/account.model";
 import { ApiError } from "@/utils/apiError";
 import connectDB from "@/utils/dbConnect";
 import { handleError } from "@/utils/handleError";
+import { AddBalanceRequestInput, addBalanceRequestSchema } from "@/zod/addBalanceRequest.schema";
 import { isValidObjectId } from "mongoose";
 
-export async function doesUserAccountExist(userId: string): Promise<boolean | IAccount> {
+async function doesUserAccountExist(
+  userId: string
+): Promise<boolean | IAccount> {
   try {
     await connectDB();
     const existingAccount = await Account.findOne({ userId });
@@ -14,18 +17,27 @@ export async function doesUserAccountExist(userId: string): Promise<boolean | IA
   }
 }
 
-export async function createAccount(userId: string) {
+async function createAccount(userId: string) {
   if (!isValidObjectId(userId)) {
     throw new ApiError("Invalid User ID", 400);
   }
-    await connectDB();
-    const existingAccount = await doesUserAccountExist(userId);
-    if(existingAccount){
-      throw new ApiError("Account already exists", 400);
-    }
-    const newAccount:IAccount = await Account.create({userId, balace: 0});
-    if(!newAccount){
-      throw new ApiError("Failed to create new user account", 500);
-    }
-    return newAccount;
+  await connectDB();
+  const existingAccount = await doesUserAccountExist(userId);
+  if (existingAccount) {
+    throw new ApiError("Account already exists", 400);
+  }
+  const newAccount: IAccount = await Account.create({ userId, balace: 0 });
+  if (!newAccount) {
+    throw new ApiError("Failed to create new user account", 500);
+  }
+  return newAccount;
 }
+
+async function addBalanceRequest(reqData: AddBalanceRequestInput) {
+  const parseResult = addBalanceRequestSchema.safeParse(reqData);
+  if(!parseResult.success){
+    throw parseResult.error;
+  }
+
+}
+export { createAccount, doesUserAccountExist, addBalanceRequest };
