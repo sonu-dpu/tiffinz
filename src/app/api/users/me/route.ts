4afd6ed1af0230b93305d51afc971b3080c72a42
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-03 06:27:41 +0000

    Refactor user retrieval API to use header-based authentication and add middleware for JWT verification

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,25 +1,20 @@
-
-import { ApiResponse } from "@/lib/ApiResponse";
+import { ApiResponse } from "@/utils/ApiResponse";
+import connectDB from "@/utils/dbConnect";
 import User from "@/models/user.model";
 import { asyncHandler } from "@/utils/asyncHandler";
-import  jwt, { JwtPayload }  from "jsonwebtoken";
-interface MyJwtPayload extends JwtPayload {
-  _id: string;
-  role?: string; 
-}
-export const GET = asyncHandler(async(req)=>{
-    const token = req.cookies.get("accessToken")?.value;
-    if(!token){
-        return ApiResponse.error("Token not found", 400)
-    }
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET!) as MyJwtPayload;
-    const userId = decodedToken?._id
-    if(!userId){
-        return ApiResponse.error("Token Expired", 400)
-    }
-    const user = await User.findById(userId).select("-password");
-    if(!user){
-        return ApiResponse.error("User not found", 404)
-    }
-    return ApiResponse.success("Fetched user details succcessfully", user)
-})
+
+export const GET = asyncHandler(async (req) => {
+  const userId = req.headers.get("x-user-id");
+  console.log(req.headers)
+  if (!userId) {
+    console.info("user id not found", userId);
+    return ApiResponse.error("Authentication required", 401);
+  }
+
+  await connectDB();
+  const user = await User.findById(userId).select("-password");
+  if (!user) {
+    return ApiResponse.error("User not found", 404);
+  }
+  return ApiResponse.success("Fetched user details succcessfully", user);
+});
---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ApiResponse } from "./utils/ApiResponse";
+import { verifyJWT } from "./utils/verifyJWT";
+
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  const token = req.cookies.get("accessToken")?.value;
+  const response = NextResponse.next();
+  if (pathname.startsWith("/api/users/login") && !token) {  
+    return NextResponse.next();
+  }
+  if (pathname.startsWith("/api")) {
+    if (!token) {
+      return ApiResponse.error("Authentication required", 401);
+    }
+    const { payload, error } = await verifyJWT(token);
+    if (error) {
+      return error;
+    }
+    response.headers.set("x-user-id", String(payload?._id));
+    response.headers.set("x-user-role", String(payload?.role));
+    console.log("payload", payload);
+  }
+  return response;
+}
+
+export const config = {
+  matcher: ["/api/(.*)"],
+};
