65a85a1f9fbd652cdb5d0b4f8a517b834e4ae44f
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-09 06:54:06 +0000

    formate

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -1,23 +1,23 @@
 import { NextResponse } from "next/server";
 import { ZodError, z } from "zod/v4";
 
-export type APIErrorDetail = ZodError | typeof Error |  Record<string, string[]> | string[] | string | unknown;
-export class ApiResponse{
-    static success<T>(message:string, data?:T, status:number=200){
-        return NextResponse.json(
-            {success:true, message, data},
-            {status}
-        );
-    }
-    static error(message: string, status: number=400, errors?: APIErrorDetail){
-        console.log('message', message)
-        return NextResponse.json(
-            {success:false, message, errors},
-            {status}
-        )
-    }
+export type APIErrorDetail =
+  | ZodError
+  | typeof Error
+  | Record<string, string[]>
+  | string[]
+  | string
+  | unknown;
+export class ApiResponse {
+  static success<T>(message: string, data?: T, status: number = 200) {
+    return NextResponse.json({ success: true, message, data }, { status });
+  }
+  static error(message: string, status: number = 400, errors?: APIErrorDetail) {
+    console.log("message", message);
+    return NextResponse.json({ success: false, message, errors }, { status });
+  }
 
-    static zodError(error: ZodError) {
+  static zodError(error: ZodError) {
     return this.error("Validation failed", 400, z.treeifyError(error));
   }
 }
--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,9 +1,7 @@
 import { handleError } from "@/utils/handleError";
 import { NextRequest } from "next/server";
 
-export function asyncHandler<T>(
-  handler: (req: NextRequest, params?:  Promise<T> ) => Promise<Response>
-) {
+export function asyncHandler<T>(handler: (req: NextRequest, params?:  Promise<T> ) => Promise<Response>) {
   return async function (req: NextRequest, context?: { params?: Promise<T> }) {
     try {
       return await handler(req, context?.params);
