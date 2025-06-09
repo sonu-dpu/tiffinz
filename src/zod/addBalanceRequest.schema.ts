5dbd042d52314dc7d682b846c698341f0f34d7d0
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-09 10:13:40 +0000

    Refactor addBalanceRequest schema

--- src/zod/addBalanceRequest.schema.ts
+++ src/zod/addBalanceRequest.schema.ts
@@ -1,11 +1,17 @@
 import { PaymentMode, PaymentStatus } from "@/constants/enum";
 import { z } from "zod/v4";
 
-export const addBalanceRequestSchema = z.object({
+const addBalanceRequestSchema = z.object({
   userId: z.string(),
-  amountAdded: z.number(),
+  amountAdded: z.number().positive("Amount must be greater than 0"),
   paymentMode:z.enum(PaymentMode),
   status: z.enum(PaymentStatus),
+  paymentScreenshot: z.url("Must be a valid URL").optional(),
+})
+const addBalanceRequestInput = addBalanceRequestSchema.extend({
+  userId:z.string().optional(),
+  status:z.enum(PaymentStatus).optional()
 })
 
-export type AddBalanceRequestInput = z.infer<typeof addBalanceRequestSchema>;
+export {addBalanceRequestInput,addBalanceRequestSchema }
+export type AddBalanceRequestInput = z.infer<typeof addBalanceRequestInput>;
