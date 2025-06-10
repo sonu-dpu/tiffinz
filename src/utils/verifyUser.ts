4c372a88dfa92df3840a8f5ee50f14a023187f70
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-10 07:38:57 +0000

    Refactor verifyUser utility by removing unused imports and the withAdminAuth function

--- src/utils/verifyUser.ts
+++ src/utils/verifyUser.ts
@@ -1,9 +1,5 @@
 import { UserRole } from "@/constants/enum";
-import { NextRequest, NextResponse } from "next/server";
-import { handleError } from "./handleError";
-
-import { verifyJWT } from "./verifyJWT";
-import { ApiError } from "./apiError";
+import { NextResponse } from "next/server";
 
 export function isAdmin(req: NextResponse) {
   const userRole = req.headers.get("x-user-role") || null;
@@ -11,30 +7,4 @@ export function isAdmin(req: NextResponse) {
   return userRole === UserRole.admin;
 }
 
-// higher order function to check if user is admin
-export function  withAdminAuth<T>(
-  callback: (req: NextRequest, params?: T) => Promise<Response>
-) {
-  return async function (req: NextRequest, context?: { params?: T }) {
-    try {
-      const token = req.cookies.get("accessToken")?.value;
-      if (!token) {
-        throw new ApiError("Authentication required", 401);
-      }
-
-      const { payload, error } = await verifyJWT(token);
-      if (error || !payload) {
-        throw error || new ApiError("Invalid token", 401);
-      }
-
-      if (payload.role !== UserRole.admin) {
-        throw new ApiError("Access denied to admin resources", 403);
-      }
-
-      return await callback(req, context?.params);
-    } catch (error) {
-      return handleError(error);
-    }
-  };
-}
 
