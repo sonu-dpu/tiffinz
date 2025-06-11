5d63b13a6e884e8fc91d514b35161ce59286e7a3
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-11 05:04:51 +0000

    refactor user verification route

---
+++ src/app/api/admin/users/[id]/verify/route.ts
@@ -0,0 +1,21 @@
+import { UserRole } from "@/constants/enum";
+import { verifyUser, verifyUsers } from "@/helpers/server/admin.user";
+import { ApiResponse } from "@/utils/ApiResponse";
+import { withAuth } from "@/utils/withAuth";
+
+// export const PATCH = withAuth(async()=>{}, {requiredRole:UserRole.admin})
+type UserIdParams = {id:string}
+export const PATCH = withAuth<UserIdParams>(async(req, params)=>{
+  const body = await req.json();
+  const userIds = body?.userIds;
+  const {id:userId} = await params as UserIdParams;
+  if(!userIds && !userId){
+    return ApiResponse.error("User id not provided", 400);
+  }
+  if(userId.trim()){
+    const {verifiedUser, userAccount} = await verifyUser(userId);
+    return ApiResponse.success("User verified and accoun created", {user: verifiedUser, account: userAccount}, 200)
+  }
+  const verifiedUsersCount = await verifyUsers(userIds);
+  return ApiResponse.success(`Verified users ${userIds.length}/${verifiedUsersCount}`, {verifiedUsersCount}, 200)
+}, {requiredRole:UserRole.admin})
