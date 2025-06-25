6602149eaab458f79c67f177c68f7a237e653d88
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-25 08:20:52 +0000

    Refactor Input component to improve error handling and add error message display

--- src/components/ui/loader-button.tsx
+++ src/components/ui/loader-button.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Button } from './button'
+import React from "react";
+import { Button } from "./button";
 
-export default function LoaderButton({isLoading=false, fallbackText="", children}:{isLoading:boolean, fallbackText:string, children:React.ReactNode}) {
+export default function LoaderButton({
+  isLoading = false,
+  fallbackText = "",
+  children,
+  className = "",
+}: {
+  isLoading: boolean;
+  fallbackText: string;
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <Button>{isLoading?fallbackText:children}</Button>
-  )
+    <Button className={className}>{isLoading ? fallbackText : children}</Button>
+  );
 }
-
