9620617ff7fa59c68bb85fd8ab5e6d3a60068d7d
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-26 08:14:52 +0000

    Refactor GET handler to improve query structure and type safety

--- src/app/api/meal-logs/route.ts
+++ src/app/api/meal-logs/route.ts
@@ -1,29 +1,31 @@
-
 import { UserRole } from "@/constants/enum";
-import { getAllMealLogs } from "@/helpers/server/meals";
+import { getAllMealLogs, ISearchQuery } from "@/helpers/server/meals";
 import { ApiResponse } from "@/utils/ApiResponse";
-import { withAuth } from "@/utils/withAuth"
+import { withAuth } from "@/utils/withAuth";
 
-export const GET = withAuth(async(req)=>{
-  const { searchParams } = req.nextUrl;
-  const userId = searchParams.get("userId") || "";
-  const username = searchParams.get("username") || "";
-  const start = searchParams.get("start") || "";
-  const end = searchParams.get("end") || "";
-  const sortType = searchParams.get("sortType") || "desc";
-  const page = parseInt(searchParams.get("page") || "1", 10);
-  const limit = parseInt(searchParams.get("limit") || "10", 10);
-  const query = {
-    userId,
-    username, 
-    start,
-    end,
-    sortType
-  } as const;
+export const GET = withAuth(
+  async (req) => {
+    const { searchParams } = req.nextUrl;
+    const userId = searchParams.get("userId") || "";
+    const username = searchParams.get("username") || "";
+    const start = searchParams.get("start") || "";
+    const end = searchParams.get("end") || "";
+    const sortType = searchParams.get("sortType") || "desc";
+    const page = parseInt(searchParams.get("page") || "1", 10);
+    const limit = parseInt(searchParams.get("limit") || "10", 10);
+    const query: ISearchQuery = {
+      userId,
+      username,
+      start,
+      end,
+      sortType: sortType === "asc" ? "asc" : "desc",
+      // ...(sortType === "acs" ? {sortType:"asc"}: {sortType:"desc"})
+    };
 
-  // console.log('query', query)
-  const mealLogs = await getAllMealLogs(query, {page, limit});
+    // console.log('query', query)
+    const mealLogs = await getAllMealLogs(query, { page, limit });
 
-  return ApiResponse.success("Fetched meal logs successfully", mealLogs)
-}, {requiredRole:UserRole.admin})
-
+    return ApiResponse.success("Fetched meal logs successfully", mealLogs);
+  },
+  { requiredRole: UserRole.admin }
+);
