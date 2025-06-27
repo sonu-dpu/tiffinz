a08802782cb06211fd6067f6e069310c706e2f8e
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-27 10:48:37 +0000

    Refactor LoginPage and user authentication functions: update LoginForm import, enhance error handling in loginUserWithPhone, and implement axios for getCurrentUser

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,15 @@
-import Login from '@/components/auth/LoginForm'
-import React from 'react'
+import LoginForm from "@/components/auth/LoginForm";
+import React from "react";
 
 function LoginPage() {
   return (
-    <div>
-      <Login></Login>
+    <div className="grid grid-cols-1 md:grid-cols-2 w-full border min-h-[100vh] items-center">
+      <div>
+        <LoginForm />
+      </div>
+      <div className="w-full h-full hidden md:block"></div>
     </div>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/helpers/client/user.auth.ts
+++ src/helpers/client/user.auth.ts
@@ -1,15 +1,15 @@
 import { handleError } from "@/lib/handleError";
 import { UserLoginWithPhoneInput } from "@/zod/user.login.schema";
 import { UserInput } from "@/zod/user.schema";
+import axios from "axios";
 
-interface IAuthUser{
-  user : UserInput | null,
+interface IAuthUser {
+  user: UserInput | null;
   error: {
     type: string;
     message: string;
   } | null;
 }
-
 
 async function registerUser(userData: UserInput) {
   try {
@@ -30,23 +30,40 @@ async function registerUser(userData: UserInput) {
   }
 }
 
-async function loginUserWithPhone(credentials: UserLoginWithPhoneInput): Promise<IAuthUser> {
+async function loginUserWithPhone(
+  credentials: UserLoginWithPhoneInput
+): Promise<IAuthUser> {
   try {
     const response = await fetch("/api/users/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(credentials),
     });
-    if( !response.ok) {
+    if (!response.ok) {
       const error = await response.json();
       throw new Error(error.message || "Login failed");
     }
     const payload = await response.json();
     const data = payload.data;
     const user = data.user;
-    return {user, error: null}
+    return { user, error: null };
   } catch (error) {
-    return {user: null, error:handleError(error, "login")};
+    return { user: null, error: handleError(error, "login") };
   }
 }
-export { registerUser, loginUserWithPhone };
+
+async function getCurrentUser(): Promise<IAuthUser> {
+  try {
+    const response = await axios.get("/api/users");
+    if (!response.data) {
+      const error = await response.data;
+      throw new Error(error.message || "Login failed");
+    }
+    const user = response.data.data.user;
+    console.log('user', user)
+    return { user, error: null };
+  } catch (error) {
+    return { user: null, error: handleError(error, "getUser") };
+  }
+}
+export { registerUser, loginUserWithPhone, getCurrentUser };
