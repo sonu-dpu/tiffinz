f6f78f96595aea260bbd85755b4ac4bb2d52e5b5
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-06-27 10:49:48 +0000

    Refactor GET route: simplify user authentication logic and remove unused imports

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,20 +1,12 @@
 import { ApiResponse } from "@/utils/ApiResponse";
-import connectDB from "@/utils/dbConnect";
-import User from "@/models/user.model";
+// import connectDB from "@/utils/dbConnect";
+// import User from "@/models/user.model";
 import { withAuth } from "@/utils/withAuth";
 
-export const GET = withAuth(async (req) => {
-  const userId = req.headers.get("x-user-id");
-  console.log(req.headers)
-  if (!userId) {
-    console.info("user id not found", userId);
-    return ApiResponse.error("Authentication required", 401);
+export const GET = withAuth(async (_req, _context, user) => {
+  console.log('user', user)
+  if(!user){
+    return ApiResponse.error("No logged in user found", 400)
   }
-
-  await connectDB();
-  const user = await User.findById(userId).select("-password");
-  if (!user) {
-    return ApiResponse.error("User not found", 404);
-  }
-  return ApiResponse.success("Fetched user details succcessfully", user);
+  return ApiResponse.success("User fetched sucessfully", {user})
 });
