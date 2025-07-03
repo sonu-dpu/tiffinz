5154f12f82dd0e82127e65913efa70faeace15de
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-07-03 06:26:35 +0000

    feat: enhance error handling in handleError function for Axios errors

--- src/lib/handleError.ts
+++ src/lib/handleError.ts
@@ -1,4 +1,13 @@
+import { AxiosError } from "axios";
+
 function handleError(error: unknown, errorType: string) {
+  if(error instanceof AxiosError){
+    console.log('axios error', error)
+    return {
+    type: errorType,
+    message: error.response?.data.message
+  };
+  }
   return {
     type: errorType,
     message: error instanceof Error ? error.message : "An error occurred while "+ errorType,
