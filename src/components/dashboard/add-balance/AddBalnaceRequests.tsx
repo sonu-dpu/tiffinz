a10c14533337416d62c71579b95ba9226533058c
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-07-16 08:53:46 +0000

    feat: refactor BalanceRequestTable to improve structure and add table headers

--- src/components/dashboard/add-balance/AddBalnaceRequests.tsx
+++ src/components/dashboard/add-balance/AddBalnaceRequests.tsx
@@ -3,16 +3,28 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Loader from "@/components/ui/Loader";
-import { Table, TableBody, TableHead } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { PaymentMode, PaymentStatus } from "@/constants/enum";
-import { getAllBalanceRequests, IAddBalanceRequestWithUser } from "@/helpers/client/add-balance";
+import {
+  getAllBalanceRequests,
+  IAddBalanceRequestWithUser,
+} from "@/helpers/client/add-balance";
 // import { ImageKitProvider } from "@imagekit/next";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 
 function AddBalnaceRequests() {
-  const [requests, setRequests] = useState<IAddBalanceRequestWithUser[] | null>(null);
+  const [requests, setRequests] = useState<IAddBalanceRequestWithUser[] | null>(
+    null
+  );
   useEffect(() => {
     async function fetchData() {
       const { data, error } = await getAllBalanceRequests();
@@ -36,12 +48,16 @@ function AddBalnaceRequests() {
               <RequestCard key={String(req._id)} request={req}></RequestCard>
             ))}
         </ImageKitProvider> */}
-        <BalanceRequestTable requests ={requests} />
+        <BalanceRequestTable requests={requests} />
       </div>
     </div>
   );
 }
-export const RequestCard = ({ request }: { request: IAddBalanceRequestWithUser }) => {
+export const RequestCard = ({
+  request,
+}: {
+  request: IAddBalanceRequestWithUser;
+}) => {
   return (
     <Card>
       <CardHeader>
@@ -70,20 +86,40 @@ export const RequestCard = ({ request }: { request: IAddBalanceRequestWithUser }
   );
 };
 
-
-const BalanceRequestTable = ({requests}:{requests:IAddBalanceRequestWithUser[]})=>{
-  if(requests?.length <= 0){
-    return <div>Not Requests Found..</div>
+const BalanceRequestTable = ({
+  requests,
+}: {
+  requests: IAddBalanceRequestWithUser[];
+}) => {
+  if (requests?.length <= 0) {
+    return <div>Not Requests Found..</div>;
   }
+  const theadings = ["SrNo.", "Id", "Name", "Email", "Amount", "Status"];
   return (
     <Table>
-      <TableHead>
+      <TableHeader>
 
-      </TableHead>
+      <TableRow>
+        {theadings.map((th) => (
+          <TableHead key={th}>{th}</TableHead>
+        ))}
+      </TableRow>
+        </TableHeader>
+
       <TableBody>
-
+        {requests.map((req, i) => (
+          <TableRow key={String(req._id) + i}>
+            <TableCell>{i + 1}</TableCell>
+            <TableCell>{String(req._id)}</TableCell>
+            <TableCell>{req.user.fullName}</TableCell>
+            <TableCell>{req.user?.email || "-"}</TableCell>
+            <TableCell>{req.amountAdded}</TableCell>
+            <TableCell>{req.status}</TableCell>
+            {/* <TableCell>{req.status}</TableCell> */}
+          </TableRow>
+        ))}
       </TableBody>
     </Table>
-  )
-}
+  );
+};
 export default AddBalnaceRequests;
