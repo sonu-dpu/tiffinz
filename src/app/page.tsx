fd9bc060db5ec350c1310e546cace8fada3e12f7
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-08-07 10:47:55 +0000

    fix: remove unnecessary div wrapper in Home component and adjust Loader styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,9 @@ import { Suspense } from "react";
 export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-full border min-h-[100vh] items-center">
-      <div>
         <Suspense fallback={<Loader />}>
           <LoginForm />
         </Suspense>
-      </div>
       <div className="w-full h-full hidden md:block"></div>
     </div>
   );
--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Loader() {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className=" w-full h-full flex justify-center items-center mx-auto">
       <Loader2 className="h-12 w-12 animate-spin duration-100"></Loader2>
     </div>
   );
