2c2a293877dfc71624216afde9bbf75e0004f181
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-08-22 10:53:52 +0000

    feat: restore AppSidebar and BottomNav components; update layout structure in RootLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,27 +1,22 @@
-"use client";
-import BottomNav from "@/components/navbar/BottomNav";
-// import Navbar from "@/components/navbar/TopNav";
-import { AppSidebar } from "@/components/ui/app-sidebar";
+
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { useAppSelector } from "@/hooks/reduxHooks";
+
 
 function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
-  const currentUser = useAppSelector((state) => state.auth.user);
+}>){
   return (
     <>
-      <AppSidebar currentUserRole={currentUser?.role} />
+      {/* <AppSidebar currentUserRole={currentUser?.role} /> */}
       <div className="w-full">
-        {/* <Navbar /> */}
         <SidebarTrigger/>
-        <main className="max-w-full w-full mx-auto">
+        <main className="max-w-full w-full mx-auto pt-10 pb-10">
           <div className="container mx-auto px-2">{children}</div>
         </main>
       </div>
-      {currentUser && <BottomNav role={currentUser?.role} />}
+  
     </>
   );
 }
