396886bfe9512b37f8d1c5fce3001f2e80944771
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-08-30 08:13:10 +0000

    feat: implement transactions API endpoint; fetch user transactions with limit option and add TransactionsPage component

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -2,8 +2,12 @@ import Transaction from "@/models/transaction.model";
 import { ApiResponse } from "@/utils/ApiResponse";
 import { withAuth } from "@/utils/withAuth";
 
-export const GET = withAuth(async (_req, _context,user )=>{
+export const GET = withAuth(async (req, _context,user )=>{
+  const searchParams = req.nextUrl.searchParams;
+  const options = {
+    limit:Number(searchParams.get("limit")),
+  }
   const userId = user?._id;
-  const transactions = await Transaction.find({user:userId})
+  const transactions = await Transaction.find({user:userId}).limit(options.limit)
   return ApiResponse.success("transactions fetched",{transactions})
 })
