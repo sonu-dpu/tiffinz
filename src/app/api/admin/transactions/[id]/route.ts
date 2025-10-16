ccf2f66ef6c662a9e45076a8d1f5970108bf414a
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-10-16 10:37:55 +0000

    feat : add get transaction by id route

---
+++ src/app/api/admin/transactions/[id]/route.ts
@@ -0,0 +1,20 @@
+import { UserRole } from "@/constants/enum"
+import { getTransactionById } from "@/helpers/server/transactions";
+import { ApiError } from "@/utils/apiError";
+import { ApiResponse } from "@/utils/ApiResponse";
+import { withAuth } from "@/utils/withAuth"
+
+type GetTransactionIdParams = {
+  id:string;
+}
+export const GET = withAuth<GetTransactionIdParams>(async (req, context)=>{
+  const {id:transactionId}  = await context.params;
+  if(!transactionId?.trim()){
+    throw new ApiError("Provide a valid transaction id");
+  }
+
+  const transaction = await getTransactionById({transactionId});
+
+  return ApiResponse.success("Transaction fetched successfully", {transaction})
+
+}, {requiredRole: UserRole.admin})
--- src/helpers/server/transactions.ts
+++ src/helpers/server/transactions.ts
@@ -2,22 +2,47 @@ import Transaction, { ITransaction } from "@/models/transaction.model";
 import { ApiError } from "@/utils/apiError";
 import connectDB from "@/utils/dbConnect";
 import { isValidObjectId } from "mongoose";
-async function createTransaction(transactionDoc:ITransaction) {
-    if(!isValidObjectId(transactionDoc.account)){
-        throw new ApiError("Inavlid Account id");
-    }
-    if(!isValidObjectId(transactionDoc.user)){
-        throw new ApiError("Inavlid user id");
-    } 
-    await connectDB();
-    const newTransaction = await Transaction.create(transactionDoc);
+async function createTransaction(transactionDoc: ITransaction) {
+  if (!isValidObjectId(transactionDoc.account)) {
+    throw new ApiError("Inavlid Account id");
+  }
+  if (!isValidObjectId(transactionDoc.user)) {
+    throw new ApiError("Inavlid user id");
+  }
+  await connectDB();
+  const newTransaction = await Transaction.create(transactionDoc);
 
-    if(!newTransaction){
-        throw new ApiError("Failed to create a transaction");
-    }
-    return newTransaction
+  if (!newTransaction) {
+    throw new ApiError("Failed to create a transaction");
+  }
+  return newTransaction;
 }
 
+async function getTransactionById({
+  transactionId,
+  userId,
+}: {
+  transactionId: string;
+  userId?: string;
+}) {
+  if (!isValidObjectId(transactionId)) {
+    throw new ApiError("Invalid Transaction ID");
+  }
+  if (userId && !isValidObjectId(userId)) {
+    throw new ApiError("Invalid User id");
+  }
+  await connectDB();
+  const query = {
+    _id: transactionId,
+    ...(userId && { user: userId }),
+  };
+  const transaction = await Transaction.findById(query)
+    .populate({path:"user", select:"-password"})
 
+  if (!transaction) {
+    throw new ApiError("Transaction not found", 404);
+  }
+  return transaction;
+}
 
-export {createTransaction}
+export { createTransaction, getTransactionById };
