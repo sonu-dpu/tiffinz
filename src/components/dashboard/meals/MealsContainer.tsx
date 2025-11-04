fbe155ebb602f5da6bb5f1794806982a1adef087
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-11-04 11:14:36 +0000

    feat: update MealCard component to include listOnly prop in MealsContainer

--- src/components/dashboard/meals/MealsContainer.tsx
+++ src/components/dashboard/meals/MealsContainer.tsx
@@ -28,7 +28,7 @@ function MealsContainer() {
         <h1 className="font-bold my-4">Meals </h1>
         <div className=" w-full grid grid-cols-1 md:grid-cols-2 gap-2 justify-center ">
           {meals.map((meal: IMeal) => (
-            <MealCard key={String(meal._id)} meal={meal} />
+            <MealCard listOnly key={String(meal._id)} meal={meal} />
           ))}
         </div>
       </div>
