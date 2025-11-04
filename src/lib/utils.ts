400d85512a337b0fea11949f1c55bb88b826ae76
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-11-04 11:13:09 +0000

    feat: add utility function to format numbers as Indian currency

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatToIndianCurrency(amount: number): string {
+  const indianCurrencyFormatter = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  });
+
+  return indianCurrencyFormatter.format(amount);
+}
