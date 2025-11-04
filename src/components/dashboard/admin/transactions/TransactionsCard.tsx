dff3cd32dcb57db462bd46e178496a6c069f85cc
Sonu Shivcharan <23521208.acs@dypvp.edu.in>
2025-11-04 11:14:47 +0000

    feat: add Avatar component using Radix UI and update TransactionsCard to display user information

--- src/components/dashboard/admin/transactions/TransactionsCard.tsx
+++ src/components/dashboard/admin/transactions/TransactionsCard.tsx
@@ -5,19 +5,20 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { TransactionType } from "@/constants/enum";
-import { ITransaction } from "@/models/transaction.model";
+import { formatToIndianCurrency } from "@/lib/utils";
+import { ITransactionWithUser } from "@/models/transaction.model";
 import { MinusIcon, PlusIcon } from "lucide-react";
-import React from "react";
 
-function TransactionsCard({ transaction }: { transaction: ITransaction }) {
+function TransactionsCard({ transaction }:{transaction: ITransactionWithUser}) {
   const transactionType = transaction.type == TransactionType.credit;
   const date = new Date(String(transaction?.createdAt)).toString().slice(0, 24);
+  const amount = formatToIndianCurrency(transaction.amount)
   return (
     <Card>
       <CardContent className="">
         <div className="flex justify-between">
           <div>
-            <CardTitle className="text">username</CardTitle>
+            <CardTitle className="text">{transaction.user.fullName}</CardTitle>
             <div className="text-xs pt-2 text-muted-foreground">
               <p >
                 Trsanction Id : {String(transaction._id)}
@@ -30,7 +31,7 @@ function TransactionsCard({ transaction }: { transaction: ITransaction }) {
             variant={transactionType ? "success" : "destructive"}
           >
             {transactionType ? <PlusIcon /> : <MinusIcon />}{" "}
-            {transaction.amount}
+            {amount}
           </Badge>
         </div>
       </CardContent>
