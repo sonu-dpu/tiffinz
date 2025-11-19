87b85d560042af545e5a39ef3477ea2445555721
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-19 22:40:04 +0530

    fix: remove unused refreshUserSession import and streamline authentication redirects

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ApiResponse } from "./utils/ApiResponse";
 import { verifyJWT } from "./utils/verifyJWT";
-import { refreshUserSession } from "./helpers/server/user.auth";
+// import { refreshUserSession } from "./helpers/server/user.auth";
 
 export async function proxy(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
@@ -13,7 +13,12 @@ export async function proxy(req: NextRequest) {
   if (pathname.startsWith("/dashboard")) {
     if (!token) {
       if (refreshToken) {
-        return await refreshUserSession(refreshToken, response);
+        return NextResponse.redirect(
+          new URL(
+            `/refresh-session?redirect=${encodeURIComponent(pathname)}`,
+            req.url
+          )
+        );
       }
       return NextResponse.redirect(
         new URL(`/login?redirect=${pathname.substring(1)}`, req.url)
@@ -23,37 +28,27 @@ export async function proxy(req: NextRequest) {
 
   if (
     pathname.startsWith("/api/users/register") ||
-    pathname.startsWith("/api/user/logout")
+    pathname.startsWith("/api/user/logout") ||
+    (pathname.startsWith("/api/users/login") && !token) ||
+    (pathname.startsWith("/api/refresh-tokens") && refreshToken)
   ) {
     return response;
   }
-  if (pathname.startsWith("/api/users/login")) {
-    return response;
-  }
-  if (pathname.startsWith("/api/refresh-tokens") && refreshToken) {
-    return response;
-  }
+
   if (pathname.startsWith("/api")) {
     console.log("inside /api pathname", pathname);
     if (!token) {
-      if (refreshToken) {
-        return await refreshUserSession(refreshToken, response);
-      }
       return ApiResponse.error("Authentication required", 401);
     }
-
     const { payload, error } = await verifyJWT(token);
     if (error || !payload?._id) {
-      return ApiResponse.error("Auhentication required", 401);
+      return ApiResponse.error("Authentication required", 401);
     }
     response.headers.set("x-user-id", String(payload?._id));
     response.headers.set("x-user-role", String(payload?.role));
-    console.log("payload", payload);
+    // console.log("payload", payload);
   }
 
-  // if(pathname.startsWith("/login") && !token) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
   return response;
 }
 
