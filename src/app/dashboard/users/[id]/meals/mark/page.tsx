5f2812600513ca05efad8bf1d4559746e13cb753
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-19 07:48:09 +0530

    feat: enhance RecordMealForm with meal selection and validation

--- src/app/dashboard/users/[id]/meals/mark/page.tsx
+++ src/app/dashboard/users/[id]/meals/mark/page.tsx
@@ -14,15 +14,20 @@ import { Input } from "@/components/ui/input";
 import {
   NativeSelect,
   NativeSelectOption,
-} from "@/components/ui/native-select"
-import { MealType } from "@/constants/enum";
+} from "@/components/ui/native-select";
+import { DailyMealFor, MealType } from "@/constants/enum";
 import { getAllMeals, GetAllMealsOptions } from "@/helpers/client/meal";
 import { useAppSelector } from "@/hooks/reduxHooks";
 import { IMeal } from "@/models/meal.model";
 import { IUser } from "@/models/user.model";
+import {
+  mealLogSchemaForAdminClient,
+  mealLogSchemaForAdminClientType,
+} from "@/zod/mealLog.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useQuery } from "@tanstack/react-query";
 import { FC } from "react";
-import { FieldValue, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 function RecordMealPage() {
   const selectedUser = useAppSelector((state) => state.users.selectedUser);
@@ -48,19 +53,24 @@ function RecordMealPage() {
 
 const options: GetAllMealsOptions = {
   isActive: true,
-  type:MealType.regular
+  type: MealType.regular,
 };
 const RecordMealForm: FC<{ user: IUser }> = ({ user }) => {
-
   const { data, error, isFetching } = useQuery({
     queryKey: ["getAllMeals", options],
     queryFn: () => getAllMeals(options),
-    refetchOnWindowFocus:false
+    refetchOnWindowFocus: false,
   });
-  const {register, handleSubmit} = useForm()
-  const handleFormSubmit = (data:FieldValue<{meal:string, user:string}>)=>{
-    console.log('data', data)
-  }
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({ resolver: zodResolver(mealLogSchemaForAdminClient) });
+  const handleFormSubmit = (formData: mealLogSchemaForAdminClientType) => {
+    setValue("user", String(user?._id));
+    console.log("data", formData);
+  };
   if (isFetching) {
     <div>Loading..</div>;
   }
@@ -68,24 +78,44 @@ const RecordMealForm: FC<{ user: IUser }> = ({ user }) => {
     return <div>{error.message}</div>;
   }
   const meals = data?.meals ?? [];
-  console.log("data", data);
+  console.log("errors", errors);
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)}>
-      {String(user._id)}
-      <Input defaultValue={user.fullName} readOnly {...register("user")} />
-      <br />
-      <NativeSelect
-      className="w-full"
-      {...register("meal")}
-    >
-      <NativeSelectOption value="">Select Meal</NativeSelectOption>
-      {meals.map((meal: IMeal) => (
-        <NativeSelectOption key={String(meal._id)} value={String(meal._id)}>
-          {meal.name} — {meal.type.toLowerCase()}
-        </NativeSelectOption>
-      ))}
-    </NativeSelect>
-    <Button>Submit</Button>
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="grid grid-cols-1 gap-2">
+      {/* {String(user._id)} */}
+      <Input
+        label="User"
+        defaultValue={String(user._id)}
+        readOnly
+        {...register("user")}
+        errorMessage={errors.user?.message}
+      />
+      <div>
+        <label htmlFor="meal">Meal</label>
+        <NativeSelect id="meal" className="max-w-fit" {...register("meal")}>
+          <NativeSelectOption className="w-full" value="">Select Meal</NativeSelectOption>
+          {meals.map((meal: IMeal) => (
+            <NativeSelectOption key={String(meal._id)} value={String(meal._id)}>
+              {meal.name} — {meal.type.toLowerCase()}
+            </NativeSelectOption>
+          ))}
+        </NativeSelect>
+        {errors.meal && <p className="text-red-400">{errors.meal.message}</p>}
+      </div>
+
+      <div>
+        <label htmlFor="mealFor">Meal</label>
+        <NativeSelect id="mealFor" {...register("mealFor")}>
+          <NativeSelectOption value="" disabled>
+            Meal For
+          </NativeSelectOption>
+          {Object.values(DailyMealFor).map((item) => (
+            <NativeSelectOption key={item} value={item}>
+              {item.toLowerCase()}
+            </NativeSelectOption>
+          ))}
+        </NativeSelect>
+      </div>
+      <Button>Submit</Button>
     </form>
   );
 };
