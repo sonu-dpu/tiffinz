4114e875c8a349e088e2d790ec34b503501857b6
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-27 21:12:49 +0530

    feat: add aggregation pipeline to getTransactionWithPopuplatedFields - partially completed

--- src/helpers/server/transactions.ts
+++ src/helpers/server/transactions.ts
@@ -1,7 +1,7 @@
 import Transaction, { ITransaction } from "@/models/transaction.model";
 import { ApiError } from "@/utils/apiError";
 import connectDB from "@/utils/dbConnect";
-import { PaginateOptions, Types } from "mongoose";
+import { PaginateOptions, PipelineStage, Types } from "mongoose";
 import { isValidObjectId } from "mongoose";
 async function createTransaction(transactionDoc: ITransaction) {
   if (!isValidObjectId(transactionDoc.account)) {
@@ -19,13 +19,7 @@ async function createTransaction(transactionDoc: ITransaction) {
   return newTransaction;
 }
 
-async function getTransactionById({
-  transactionId,
-  userId,
-}: {
-  transactionId: string;
-  userId?: string;
-}) {
+async function getTransactionById(transactionId: string, userId?: string) {
   if (!isValidObjectId(transactionId)) {
     throw new ApiError("Invalid Transaction ID");
   }
@@ -39,7 +33,7 @@ async function getTransactionById({
   };
   const transaction = await Transaction.findOne(query)
     .populate("mealLog")
-    .populate({ path: "mealLog", populate: { path: "meal"} })
+    .populate({ path: "mealLog", populate: { path: "meal" } })
     .populate({ path: "mealLog", populate: { path: "extras.extras" } });
   if (!userId) {
     await transaction?.populate({ path: "user", select: "-password" });
@@ -63,4 +57,93 @@ async function getUserTransactions(userId: string, options: PaginateOptions) {
   return transactions;
 }
 
-export { createTransaction, getTransactionById, getUserTransactions };
+// using agggregation pipeline in this function
+async function getTransactionWithPopuplatedFields(
+  transactionId: string,
+  userId?: string
+) {
+  if (!isValidObjectId(transactionId)) {
+    throw new ApiError("Invalid Transaction ID");
+  }
+  if (userId && !isValidObjectId(userId)) {
+    throw new ApiError("Invalid User ID");
+  }
+  await connectDB();
+  const pipeline: PipelineStage[] = [
+    {
+      $match: {
+        _id: new Types.ObjectId(transactionId),
+        ...(userId && { user: new Types.ObjectId(userId) }),
+      },
+    },
+    ...(userId
+      ? [
+          {
+            $lookup: {
+              from: "users",
+              localField: "user",
+              foreignField: "_id",
+              as: "user",
+              pipeline: [
+                {
+                  $project: {
+                    _id: 1,
+                    fullname: 1,
+                    email: 1,
+                    role: 1,
+                  },
+                },
+              ],
+            },
+          },
+        ]
+      : []),
+    {
+      $lookup: {  
+        from: "meallogs",
+        localField: "mealLog",
+        foreignField: "_id",
+        as: "mealLog",
+        pipeline: [
+          {
+            $lookup: {
+              from: "meals",
+              localField: "meal",
+              foreignField: "_id",
+              as: "meal",
+            },
+          },
+          {
+            $lookup: {
+              from: "meals",
+              localField: "extras.extras",
+              foreignField: "_id",
+              as: "populatedExtras",
+            },
+          },
+          {
+            $addFields:{
+              extras:{
+                $map:{
+                  input: "$extras",
+                  as: "extraItem",
+                  in:{
+                    quantity: "$$extraItem.quantity",
+                  },
+                },
+              }
+            }
+          }
+          
+        ],
+      },
+    },
+  ];
+  const transaction = await Transaction.aggregate(pipeline);
+  if (transaction.length === 0) {
+    throw new ApiError("Transaction not found", 404);
+  }
+  return transaction[0];
+}
+
+export { createTransaction, getTransactionById, getUserTransactions, getTransactionWithPopuplatedFields };
