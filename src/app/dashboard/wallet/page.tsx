10c5f1410b5dd03c8584634ab808f1c35c9433a3
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-29 21:36:04 +0530

    remove unnecessary imports

--- src/app/dashboard/wallet/page.tsx
+++ src/app/dashboard/wallet/page.tsx
@@ -1,6 +1,5 @@
 import { AccountCard } from "@/components/dashboard";
 import UserTransactions from "@/components/dashboard/transactions/UserTransactions";
-import React from "react";
 
 function WalletPage() {
   return (
