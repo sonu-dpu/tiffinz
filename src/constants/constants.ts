73478657be87be7f419b1c8ee10e52377316d0ae
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-30 21:46:21 +0530

    add DB_NAME to .env

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-const DB_NAME = "Tifinz";
+const DB_NAME = process.env.DB_NAME;
 const DEFAULT_AVATAR_URI = ""
 const IMAGEKIT_FOLDER = "tifinz"; // Folder in ImageKit where files will be uploaded
 
