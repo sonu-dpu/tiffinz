b62c5a8bdc4876cca44da004de5b840fb8d297d0
sonu-shivcharan <sonushivcharan@outlook.com>
2025-11-30 14:57:18 +0530

    fix logout redirect bug

--- src/app/(auth)/logout/page.tsx
+++ src/app/(auth)/logout/page.tsx
@@ -7,7 +7,7 @@ import { logout } from "@/store/authSlice";
 import { logoutUser } from "@/helpers/client/user.auth";
 import { toast } from "sonner";
 import Loader from "@/components/ui/Loader";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import useCurrentUser from "@/hooks/useCurrentUser";
 
 const LogoutPage = () => {
@@ -19,14 +19,16 @@ const LogoutPage = () => {
     refetchOnWindowFocus: false,
     retry: false,
   });
+  const router = useRouter()
+
   useEffect(() => {
     if (isFetched) {
       dispatch(logout());
-       const message =  logoutSuccess ? "Logout successful" : "User already logged out";
-      toast.success(message)
     }
     if (!isLoggedIn) {
-      redirect("/login")
+      router.push("/login");
+       const message =  logoutSuccess ? "Logout successful" : "User already logged out";
+      toast.success(message)
     }
   }, [isFetched, dispatch, isLoggedIn]);
 
