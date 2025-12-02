d4bde5d6b10e5c23a537240edfb1a6080284fe72
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-02 08:56:54 +0530

    fix : layout on mobile screens

--- src/app/dashboard/meals/add/page.tsx
+++ src/app/dashboard/meals/add/page.tsx
@@ -1,7 +1,7 @@
 import AddMealForm from "@/components/dashboard/meals/AddNewMealForm";
 
 function page() {
-  return <AddMealForm className=" w-md mx-auto" />;
+  return <AddMealForm className="w-full max-w-md" />;
 }
 
 export default page;
