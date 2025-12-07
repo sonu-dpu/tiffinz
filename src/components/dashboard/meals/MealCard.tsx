54b1762d66fd847a422519869edc72b5658e3c84
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-07 15:54:19 +0530

    feat: add confirm prompt before deleting meal

--- src/components/dashboard/meals/MealCard.tsx
+++ src/components/dashboard/meals/MealCard.tsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 import { IMeal } from "@/models/meal.model";
 import {
   Card,
   CardAction,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 import { getDateAndTimeString } from "@/lib/getDateAndTimeString";
 import { EditIcon, Ellipsis, Trash2 } from "lucide-react";
 
@@ -23,6 +21,10 @@ import {
 import { WithAlert } from "@/components/WithAlert";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { UserRole } from "@/constants/enum";
+import { QueryClientContext, useMutation } from "@tanstack/react-query";
+import { deleteMealById } from "@/helpers/client/admin.meals";
+import { toast } from "sonner";
+import { useContext } from "react";
 function MealCard({
   meal,
   listOnly = false,
@@ -30,51 +32,64 @@ function MealCard({
   meal: IMeal;
   listOnly?: boolean;
 }) {
-  const {userRole} = useCurrentUser();
+  const { userRole } = useCurrentUser();
   return (
-    <Link href={`/dashboard/meals/${meal._id}`}>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            {meal.name} - {meal.type}
-          </CardTitle>
-          <CardAction>
-            {
-              userRole===UserRole.admin && <MealActionMenu />
-            }
-          </CardAction>
-          <Badge color={meal.isActive ? "green" : "red"}>
-            {meal.isActive ? "Active" : "Inactive"}
-          </Badge>
-        </CardHeader>
+    // <Link href={`/dashboard/meals/${meal._id}`}>
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          {meal.name} - {meal.type}
+        </CardTitle>
+        <CardAction>
+          {userRole === UserRole.admin && <MealActionMenu meal={meal} />}
+        </CardAction>
+        <Badge color={meal.isActive ? "green" : "red"}>
+          {meal.isActive ? "Active" : "Inactive"}
+        </Badge>
+      </CardHeader>
 
-        <CardContent>
-          <p>
-            <strong>Price:</strong> ₹{meal.price}
-          </p>
-          <p>
-            <strong>Description:</strong> {meal.description}
-          </p>
-          <p>
-            <small>Created at: {getDateAndTimeString(meal.createdAt!)}</small>
-          </p>
-          {/* {!listOnly && (
+      <CardContent>
+        <p>
+          <strong>Price:</strong> ₹{meal.price}
+        </p>
+        <p>
+          <strong>Description:</strong> {meal.description}
+        </p>
+        <p>
+          <small>Created at: {getDateAndTimeString(meal.createdAt!)}</small>
+        </p>
+        {/* {!listOnly && (
           <CardFooter className="flex justify-end">
             <Link href={`/dashboard/meals/${meal._id}`}>
               <Button disabled={!meal.isActive}>Order Now</Button>
             </Link>
           </CardFooter> */}
-          {/* )} */}
-        </CardContent>
-      </Card>
-    </Link>
+        {/* )} */}
+      </CardContent>
+    </Card>
+    // </Link>
   );
 }
 
-
-
-function MealActionMenu() {
-
+function MealActionMenu({ meal }: { meal: IMeal }) {
+  const client = useContext(QueryClientContext);
+  const deleteMealMutation = useMutation({
+    mutationFn: (mealId: string) => {
+      toast.loading("Deleting meal...", { id: "delete-meal" });
+      return deleteMealById(mealId);
+    },
+    onSuccess(_) {
+      toast.dismiss("delete-meal");
+      client?.invalidateQueries({
+        queryKey: ["getAllMeals"],
+      });
+      toast.success("Meal deleted successfully");
+    },
+    onError: (error) => {
+      toast.dismiss("delete-meal");
+      toast.error(error.message || "Something went wrong");
+    },
+  });
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -83,16 +98,27 @@ function MealActionMenu() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="max-w-20">
-        {/* <DropdownMenuLabel>Appearance</DropdownMenuLabel> */}
-
         <DropdownMenuItem>
           Edit
           <EditIcon />
         </DropdownMenuItem>
         <DropdownMenuItem>Toggle Status</DropdownMenuItem>
-        <DropdownMenuItem>
-          <WithAlert title="Confirm to delete the meal" trigger={<Button variant={"destructive"}>Delete<Trash2/></Button>}>
-          <> </>
+        <DropdownMenuItem asChild>
+          <WithAlert
+            title="Confirm to delete the meal"
+            onConfirm={() => deleteMealMutation.mutate(String(meal._id))}
+            trigger={
+              <Button className="w-full bg-transparent" variant={"destructive"}>
+                Delete
+                <Trash2 />
+              </Button>
+            }
+          >
+            <div className="text-muted-foreground">
+              <p>Are you sure you want to delete the meal?</p>
+              <p>{meal.name} </p>
+              <p> This action cannot be undone.</p>
+            </div>
           </WithAlert>
         </DropdownMenuItem>
       </DropdownMenuContent>
