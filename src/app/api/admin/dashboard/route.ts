f3369c3860dcaf26d4fb5c567ef6921bcbcd5878
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-09 23:32:52 +0530

    add api route to get the total monthly credited amount

---
+++ src/app/api/admin/dashboard/route.ts
@@ -0,0 +1,40 @@
+import { TransactionType, UserRole } from "@/constants/enum";
+import Transaction from "@/models/transaction.model";
+import { ApiResponse } from "@/utils/ApiResponse";
+import { withAuth } from "@/utils/withAuth";
+
+export const GET = withAuth(
+  async () => {
+    const totalMoneyRecievedMonthly = await Transaction.aggregate([
+      {
+        $match: {
+          type: TransactionType.credit,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+          },
+          totalAmount: { $sum: "$amount" },
+        },
+      },
+      {
+        $addFields: {
+          year: "$_id.year",
+          month: "$_id.month",
+          _id: "$$REMOVE",
+        },
+      },
+      {
+        $sort: {
+          year: 1,
+          month: 1,
+        },
+      },
+    ]);
+    return ApiResponse.success("Admin route", { totalMoneyRecievedMonthly });
+  },
+  { requiredRole: UserRole.admin }
+);
