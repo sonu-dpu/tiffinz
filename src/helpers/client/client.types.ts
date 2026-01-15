f8fbbad84b6e6eb8720f425a553c13bba84a7113
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-15 21:18:24 +0530

    refactor client types for improved readability and structure

--- src/helpers/client/client.types.ts
+++ src/helpers/client/client.types.ts
@@ -1,36 +1,38 @@
 import { IMeal } from "@/models/meal.model";
 import { IMealLog } from "@/models/mealLogs.model";
+import { ITransactionWithUser } from "@/models/transaction.model";
 import { IUser } from "@/models/user.model";
 import { AggregatePaginateResult } from "mongoose";
 
-export type helperResponse<T=unknown> = {
+export type helperResponse<T = unknown> = {
   data: T | null;
   error: {
     type: string;
     message: string;
   } | null;
 };
-export type ExtraMealType={
-      _id:string,
-      name:string,
-      price:number,
-      description:string
-}
+export type ExtraMealType = {
+  _id: string;
+  name: string;
+  price: number;
+  description: string;
+};
 
 // meal log with meal details
 export type MealLogPopulatedType = Omit<IMealLog, "extras"> & {
-  meal:IMeal;
-  user:IUser;
-  extras?:{
-    quantity:number,
-    details:ExtraMealType
-  }[],
-  priceBreakdown:{
-    baseAmount:number,
-    extrasTotal?:number,
-    total:number,
-  }
-}
-
-export type PaginatedResult<T> = AggregatePaginateResult<T>
-
+  meal: IMeal;
+  user: IUser;
+  extras?: {
+    quantity: number;
+    details: ExtraMealType;
+  }[];
+  priceBreakdown: {
+    baseAmount: number;
+    extrasTotal?: number;
+    total: number;
+  };
+};
+export type TransactionWithMealLog = ITransactionWithUser & {
+  mealLog: IMealLog;
+};
+export type PaginatedResult<T> = AggregatePaginateResult<T>;
