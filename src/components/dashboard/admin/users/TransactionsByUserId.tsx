0554108eb1933b3210a3b71d040df903c6f8038d
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-16 21:44:30 +0530

    Change the Transactio typ with its correct type

--- src/components/dashboard/admin/users/TransactionsByUserId.tsx
+++ src/components/dashboard/admin/users/TransactionsByUserId.tsx
@@ -9,12 +9,12 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Loader from "@/components/ui/Loader";
-import { ITransaction } from "@/models/transaction.model";
 import { toast } from "sonner";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { TransactionItem } from "../../transactions/UserTransactions";
+import { TransactionWithMealLog } from "@/helpers/client/client.types";
 
 function TransactionsByUserId({ userId }: { userId: string }) {
   const {
@@ -42,7 +42,7 @@ function TransactionsByUserId({ userId }: { userId: string }) {
         <CardTitle>Transactions</CardTitle>
       </CardHeader>
       <CardContent className="p-0 mt-0 border-t">
-        {transactions.map((transaction: ITransaction) => (
+        {transactions.map((transaction: TransactionWithMealLog) => (
           <TransactionItem
             key={String(transaction._id)}
             transaction={transaction}
