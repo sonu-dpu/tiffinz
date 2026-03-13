65b030e46459a4c29ffa9030ee6ea0e8a5414ba0
sonu-shivcharan <sonushivcharan@outlook.com>
2026-03-13 21:54:48 +0530

    refactor: improve code formatting and enhance session redirection logic

--- src/app/(auth)/refresh-session/page.tsx
+++ src/app/(auth)/refresh-session/page.tsx
@@ -1,41 +1,50 @@
-"use client"
+"use client";
 import Loader from "@/components/ui/Loader";
 import { refreshUserSession } from "@/helpers/client/user.auth";
 import { login } from "@/store/authSlice";
 import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { toast } from "sonner";
 
-
-
 function RefreshPage() {
-  const {data:user, error, isFetching} = useQuery({
+  const {
+    data: user,
+    error,
+    isFetching,
+  } = useQuery({
     queryKey: ["refreshUserSession"],
     queryFn: refreshUserSession,
-    retry: false
+    retry: false,
   });
   const dispatch = useDispatch();
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get("redirect") || "/dashboard";
   useEffect(() => {
     if (user) {
       dispatch(login(user));
-      router.push("/dashboard");
+      router.replace(`${decodeURIComponent(redirectTo)}`);
     } else if (error) {
       toast.error("Failed to refresh session: " + error.message);
-      router.push("/login");
+      router.push(`/login?redirect=${encodeURIComponent(redirectTo)}`);
     }
-  }, [user, error, dispatch, router, isFetching]);
-  if(error){
-    toast.error(error.message)
+
+    if (isFetching) {
+      toast.loading("Refreshing session...");
+    }
+    return () => {
+      toast.dismiss();
+    };
+  }, [user, error, dispatch, router, isFetching, redirectTo]);
+  if (error) {
+    toast.error(error.message);
   }
-  if (isFetching) {
-    return <Loader />;
-  }
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center min-h-screen w-full">
       <Loader />
     </div>
   );
