0bc11a68b6f18098bf4ec1b746923110a5b27844
sonu-shivcharan <sonushivcharan@outlook.com>
2026-03-13 22:07:23 +0530

    refactor: remove unused Geist_Mono import and simplify body class

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers/providers";
 import { Toaster } from "@/components/ui/sonner";
 import InstallPrompt from "@/components/ui/pwa-install-button";
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -26,9 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.className} ${geistMono.variable} antialiased bg-background`}
-      >
+      <body className={`${geistSans.className} antialiased bg-background`}>
         <Providers>{children}</Providers>
         <Toaster position="top-right" />
         <InstallPrompt />
