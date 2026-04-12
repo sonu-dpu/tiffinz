63d8bb77fc5a4dfa0768519fe49584d33b343a0d
sonu-shivcharan <sonushivcharan@outlook.com>
2026-04-12 13:42:20 +0530

    format

--- src/app/api/meals/[mealId]/route.ts
+++ src/app/api/meals/[mealId]/route.ts
@@ -1,25 +1,19 @@
 import { ApiResponse } from "@/utils/ApiResponse";
-import {
-  getMealById,
-} from "@/helpers/server/meals";
+import { getMealById } from "@/helpers/server/meals";
 import { ApiError } from "@/utils/apiError";
 import { withAuth } from "@/utils/withAuth";
-
 
 type MealParams = { mealId: string };
 export const GET = withAuth<MealParams>(
   async (_req, context, user) => {
-    const { mealId } = await context.params
+    const { mealId } = await context.params;
     if (!mealId) {
       throw new ApiError("Meal id not found", 400);
     }
-    console.log('user', user)
+    console.log("user", user);
     const meal = await getMealById(mealId);
+
     return ApiResponse.success("Meal fetched Successfully", { meal });
   },
   // { requiredRole: UserRole.user }
 );
-
-
-
-
