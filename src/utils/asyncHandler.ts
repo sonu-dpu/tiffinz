121e7e2a23e2162d1ea56ba4b553199ad8b8897f
sonu-shivcharan <sonushivcharan@outlook.com>
2026-04-12 12:33:40 +0530

    remove error logs and format code

--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -4,10 +4,7 @@ type RouteContext<T = Record<string, never>> = {
   params: Promise<T>;
 };
 export function asyncHandler<T = Record<string, never>>(
-  handler: (
-    req: NextRequest,
-    context: RouteContext<T>
-  ) => Promise<Response>
+  handler: (req: NextRequest, context: RouteContext<T>) => Promise<Response>,
 ) {
   return async function (req: NextRequest, context: RouteContext<T>) {
     try {
--- src/utils/handleError.ts
+++ src/utils/handleError.ts
@@ -1,19 +1,18 @@
 import { MongooseError } from "mongoose";
 import { ApiResponse } from "@/utils/ApiResponse";
 import { ApiError } from "./apiError";
-import  {JOSEError} from "jose/errors";
+import { JOSEError } from "jose/errors";
 
 export function handleError(error: unknown) {
-  console.log('error', error)
+  console.log("error", error);
   if (error instanceof MongooseError) {
     return ApiResponse.error(error.message, 400);
   }
-  if(error instanceof ApiError){
-    console.log("Api ERROR  : ", error )
-    return ApiResponse.error(error.message, error.statusCode, error.errors)
+  if (error instanceof ApiError) {
+    return ApiResponse.error(error.message, error.statusCode, error.errors);
   }
-  if(error instanceof JOSEError ){
-    return ApiResponse.error(error.message, 401, error.stack)
+  if (error instanceof JOSEError) {
+    return ApiResponse.error(error.message, 401, error.stack);
   }
   const message =
     error instanceof Error ? error.message : "Internal server error";
