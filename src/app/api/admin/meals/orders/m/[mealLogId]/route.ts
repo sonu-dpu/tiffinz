6c7a5b43fa26acd78b08057dcdbb156c2c25cd54
sonu-shivcharan <sonushivcharan@outlook.com>
2026-04-13 12:13:54 +0530

    feat: add routes for getting meal log by ID and updating meal log status with transaction handling

---
+++ src/app/api/admin/meals/orders/m/[mealLogId]/route.ts
@@ -0,0 +1,87 @@
+import { MealStatus, TransactionType, UserRole } from "@/constants/enum";
+import { updateAccountBalance } from "@/helpers/server/admin.accounts";
+import { getMealLogById } from "@/helpers/server/meals";
+import { createTransactionDoc } from "@/helpers/server/transactions";
+import Account from "@/models/account.model";
+import MealLog from "@/models/mealLogs.model";
+import { ApiError } from "@/utils/apiError";
+import { ApiResponse } from "@/utils/ApiResponse";
+import connectDB from "@/utils/dbConnect";
+import { withAuth } from "@/utils/withAuth";
+import { isValidObjectId, startSession } from "mongoose";
+
+type MealLogParam = {
+  mealLogId: string;
+};
+const getMealLogByIdRoute = withAuth<MealLogParam>(
+  async (req, context) => {
+    const { mealLogId } = await context.params;
+
+    const mealLog = await getMealLogById(mealLogId);
+    return ApiResponse.success("Hello", { order: mealLog });
+  },
+  { requiredRole: UserRole.admin },
+);
+
+const updateMealLogStatusRoute = withAuth<MealLogParam>(
+  async (req, context) => {
+    const { mealLogId } = await context.params;
+    const body = await req.json();
+    const status = body.status as MealStatus;
+    if (!isValidObjectId(mealLogId)) {
+      throw new ApiError("Invalid meal log id");
+    }
+    if (!(status.toLocaleLowerCase() in MealStatus)) {
+      throw new ApiError("Invalid status");
+    }
+
+    await connectDB();
+    const mealLog = await MealLog.findById(mealLogId);
+    if (!mealLog) {
+      throw new ApiError("Meal log not found", 404);
+    }
+    const session = await startSession();
+    try {
+      // if status is taken create a transaction and then upadte the user account balance
+      if (status === MealStatus.taken) {
+        session.startTransaction();
+        mealLog.status = status.toLocaleUpperCase() as MealStatus;
+        const account = await Account.findOne({ user: mealLog.user }).session(
+          session,
+        );
+        if (!account) {
+          throw new ApiError("Account not found for the user", 404);
+        }
+        const newTransaction = await createTransactionDoc({
+          data: {
+            account: account._id,
+            user: mealLog.user,
+            amount: mealLog.totalAmount,
+            type: TransactionType.debit,
+            mealLog: mealLog._id,
+            isMeal: true,
+          },
+          session,
+        });
+        await updateAccountBalance({
+          accountId: account,
+          amount: newTransaction.amount,
+          type: newTransaction.type,
+          session,
+        });
+      }
+      await mealLog.save({ session });
+      await session.commitTransaction();
+      return ApiResponse.success("Meal log status updated successfully", {
+        mealLog,
+      });
+    } catch (error) {
+      await session.abortTransaction();
+      console.error("Failed to update meal log status");
+      throw error;
+    }
+  },
+  { requiredRole: UserRole.admin },
+);
+
+export { getMealLogByIdRoute as GET, updateMealLogStatusRoute as PATCH };
