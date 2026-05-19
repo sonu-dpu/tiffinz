8a816be693242efe4f69f3ecc30fdb35910e0ea7
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-19 20:53:33 +0530

    update

--- src/components/dashboard/UserDashboard.tsx
+++ src/components/dashboard/UserDashboard.tsx
@@ -1,15 +1,13 @@
 import { IUser } from "@/models/user.model";
-import UserTransactions from "./transactions/UserTransactions";
 
 function UserDashboard({ user }: { user: IUser }) {
   return (
-    <div className="px-2 pb-8">
+    <div className="px-2 py-4">
       <div>
         <h1 className="text-2xl font-semibold mb-2">
           Welcome, {user.fullName}!
         </h1>
       </div>
-      <UserTransactions />
     </div>
   );
 }
