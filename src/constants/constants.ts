c5ce10720652b8e20355e53fffcfada834b197d0
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-21 15:34:20 +0530

    fix: correct formatting

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,5 @@
 const DB_NAME = process.env.DB_NAME;
-const DEFAULT_AVATAR_URI = ""
+const DEFAULT_AVATAR_URI = "";
 const IMAGEKIT_FOLDER = "tifinz"; // Folder in ImageKit where files will be uploaded
 
-
-export {DB_NAME, DEFAULT_AVATAR_URI, IMAGEKIT_FOLDER};
+export { DB_NAME, DEFAULT_AVATAR_URI, IMAGEKIT_FOLDER };
