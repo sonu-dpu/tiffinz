75edeb366831a84485f308ef7dd13a4ce9192092
sonu-shivcharan <sonushivcharan@outlook.com>
2026-05-21 15:34:35 +0530

    refactor: clean up BottomNav component by removing unused imports and improving formatting

--- src/components/navbar/BottomNav.tsx
+++ src/components/navbar/BottomNav.tsx
@@ -4,9 +4,6 @@ import { usePathname, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils"; // Optional utility for merging classNames
 import {
   Home,
-  User,
-  CreditCard,
-  Settings,
   List,
   Users,
   BarChart2,
@@ -29,44 +26,48 @@ interface NavItem {
 //   role?: UserRole;
 // }
 
-  const userNav: NavItem[] = [
-    { label: "Home", icon: <Home size={20} />, path: "/dashboard" },
-    // { label: "Account", icon: <User size={20} />, path: "/dashboard/account" },
-    {
-      label: "Wallet",
-      icon: <Wallet size={20} />,
-      path: "/dashboard/wallet",
-    },
-    {label:"Profile", icon:<UserCircle size={20}/>, path:"/dashboard/profile"}
-    // { label: "My Meals", icon: <List size={20} />, path: "/my-meals" },
-    // { label: "Settings", icon: <Settings size={20} />, path: "/settings" },
-  ];
+const userNav: NavItem[] = [
+  { label: "Home", icon: <Home size={20} />, path: "/dashboard" },
+  // { label: "Account", icon: <User size={20} />, path: "/dashboard/account" },
+  {
+    label: "Wallet",
+    icon: <Wallet size={20} />,
+    path: "/dashboard/wallet",
+  },
+  {
+    label: "Profile",
+    icon: <UserCircle size={20} />,
+    path: "/dashboard/profile",
+  },
+  // { label: "My Meals", icon: <List size={20} />, path: "/my-meals" },
+  // { label: "Settings", icon: <Settings size={20} />, path: "/settings" },
+];
 
-  const adminNav: NavItem[] = [
-    { label: "Home", icon: <Home size={20} />, path: "/dashboard" },
-    { label: "Users", icon: <Users size={20} />, path: "/dashboard/users" },
-    { label: "Meals", icon: <List size={20} />, path: "/dashboard/meals" },
-    {
-      label: "Requests",
-      icon: <Receipt size={20} />,
-      path: "/dashboard/requests",
-    },
-    {
-      label: "Analytics",
-      icon: <BarChart2 size={20} />,
-      path: "/dashboard/meals/add",
-    },
-    // {
-    //   label: "Settings",
-    //   icon: <Settings size={20} />,
-    //   path: "/dashboard/settings",
-    // },
-  ];
+const adminNav: NavItem[] = [
+  { label: "Home", icon: <Home size={20} />, path: "/dashboard" },
+  { label: "Users", icon: <Users size={20} />, path: "/dashboard/users" },
+  { label: "Meals", icon: <List size={20} />, path: "/dashboard/meals" },
+  {
+    label: "Requests",
+    icon: <Receipt size={20} />,
+    path: "/dashboard/requests",
+  },
+  {
+    label: "Analytics",
+    icon: <BarChart2 size={20} />,
+    path: "/dashboard/meals/add",
+  },
+  // {
+  //   label: "Settings",
+  //   icon: <Settings size={20} />,
+  //   path: "/dashboard/settings",
+  // },
+];
 
 export default function BottomNav() {
   const pathname = usePathname();
   const router = useRouter();
-  const {user,userRole:role}=useCurrentUser()
+  const { userRole: role } = useCurrentUser();
   const isMobile = useIsMobile();
   const navItems = role === UserRole.admin ? adminNav : userNav;
   if (
@@ -91,7 +92,7 @@ export default function BottomNav() {
               className={cn(
                 "flex my-2 max-w-[60px] w-full p-1 flex-col items-center text-xs cursor-pointer transition-all border border-transparent rounded-md duration-150",
                 "text-muted-foreground hover:bg-primary/10 hover:text-primary",
-                isActive && "  text-green-700 rounded-lg"
+                isActive && "  text-green-700 rounded-lg",
               )}
             >
               {item.icon}
